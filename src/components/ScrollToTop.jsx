cc35a2450db3d4e1dc3a8ee3570b148c8750aead
muskan <chaurasiyamushkan3@gmail.com>
2026-08-09 11:20:09 +0530

    commit

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,12 +1,18 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.history.scrollRestoration = "manual";
-    window.scrollTo(0, 0);
+  useLayoutEffect(() => {
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
   }, [pathname]);
 
   return null;
